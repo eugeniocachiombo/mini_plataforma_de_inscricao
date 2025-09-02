bbb93d294f4aacc532dd01acc5fc109e5f210e09
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-09-02 21:04:55 +0100

    feat: created and configured Candidature

---
+++ app/Models/Candidature.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Candidature extends Model
+{
+    protected $fillable = [
+        "code",
+        "program_id",
+        "user_id",
+    ];
+}
