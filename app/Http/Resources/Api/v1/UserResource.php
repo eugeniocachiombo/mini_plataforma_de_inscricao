158638359dbc092894ca28408a88b32b474a5d6a
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-09-02 21:44:10 +0100

    fix: bug word "request" to "this"

--- app/Http/Resources/Api/v1/UserResource.php
+++ app/Http/Resources/Api/v1/UserResource.php
@@ -15,9 +15,9 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            "id" => $request->id,
-            "name" => $request->name,
-            "email" => $request->email,
+            "id" => $this->id,
+            "name" => $this->name,
+            "email" => $this->email
         ];
     }
 }
