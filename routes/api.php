9124cf56eda22240a38b418aa2fdcbaacc66162b
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-09-03 01:01:07 +0100

    feat: route is created

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\v1\CandidatureController;
+use App\Http\Controllers\Api\v1\ProgramController;
+use App\Http\Controllers\Api\v1\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('candidatos', UserController::class);
+	Route::apiResource('programas', ProgramController::class);
+    Route::apiResource('candidaturas', CandidatureController::class);
+});
